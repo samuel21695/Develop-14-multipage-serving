f8e93940a23261e52d34448e877e958b59628c26
samuel21695 <samuel21695@gmail.com>
2023-10-06 10:10:28 +0900

    write function docMaker

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hi')
+const http = require("http");
+
+const server = http.createServer(function(request, response) {
+  console.log(request.url)
+
+  function docMaker(title, href, text) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}</title>
+    </head>
+    <body>
+      <a href="${href}">${text}</a>
+    </body>
+    </html>
+    `;
+  }
+})
